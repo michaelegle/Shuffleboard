234045091b5e06f0dc2b69d9f2832b56f43f6338
Michael Egle <59934818+michaelegle@users.noreply.github.com>
2026-05-18 22:50:33 -0500

    updated training data, trained new model

---
+++ Code/train_stone_detection_model.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import torch
+import multiprocessing
+
+# Load a pretrained keypoint model
+model = YOLO("yolov8n.pt")
+
+print(torch.cuda.is_available())
+
+def train_model():
+    # Train
+    model.train(
+        data="../Models/stone_detection/data.yaml",
+        name = "../Models/stone_detection/model_saves",
+        imgsz=640,
+        epochs=100,
+        batch=16,
+        lr0=0.001,
+        hsv_h=0.0,  # disable heavy augmentation
+        hsv_s=0.0,
+        hsv_v=0.0,
+        device=0
+    )
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target = train_model)
+    p.start()
+    p.join()
+
